6828fc562cd702e029dcdcd8212bb9d62b431100
Anitaachu <anitatom20@gmail.com>
2020-05-03 00:02:50 +0100

    Create user.py

---
+++ user.py
@@ -0,0 +1,9 @@
+Name = input("What is your name: ")
+Ambition = input("What is your Ambition/career path: ")
+Reason = input("What are you reasons for choosing your career path: ")
+Favorite_quotes = input("What is your favourite quote: ")
+
+print("Hello Everyone, My name is " + Name)
+print("I want to be a good " + Ambition + " someday " + Reason)
+print("My favorite quotes " + Favorite_quotes + " I love this quote because it pushes me to be better and do better.")
+print("Thank you for listening!")
