ebb6a49ddcf2326043937b05091609d2477f23a6
Anitaachu <anitatom20@gmail.com>
2020-05-05 17:13:47 +0100

    Update user.py

--- user.py
+++ user.py
@@ -1,9 +1,14 @@
-Name = input("What is your name: ")
-Ambition = input("What is your Ambition/career path: ")
-Reason = input("What are you reasons for choosing your career path: ")
-Favorite_quotes = input("What is your favourite quote: ")
+"""This is a Madlibs Project."""
 
-print("Hello Everyone, My name is " + Name)
-print("I want to be a good " + Ambition + " someday " + Reason)
-print("My favorite quotes " + Favorite_quotes + " I love this quote because it pushes me to be better and do better.")
+name = input("What is your name: ")
+ambition = input("What is your Ambition/career path: ")
+reason = input("What are you reasons for choosing your career path: ")
+favorite_quote = input("What is your favourite quote: ")
+
+print("Hello Everyone, My name is " + name)
+print("I want to be really good in " + ambition + " someday, " + reason)
+
+print("My favorite quote is " + favorite_quote +
+      " I love this quote because it pushes"
+      " me to be better and do better.")
 print("Thank you for listening!")
